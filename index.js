59c0458c0de6a33a0d9ddf4f6bcc8b1f094880fa
Sharon Njoroge <waithirasharon222@gmail>
2025-09-28 17:23:26 +0300

    add work

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const tutorials = [
   'The Test Driven Development Workflow',
   'What is NaN and how Can we Check for it',
   'What is the difference between stopPropagation and preventDefault?',
-        'Immutable State and Pure Functions',
+        
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
     'what is JSONP?'
