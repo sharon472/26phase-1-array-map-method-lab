6b1b59c8427ed3bc877b14c138bd3895300cc6f7
Sharon Njoroge <waithirasharon222@gmail>
2025-09-28 17:20:23 +0300

    add work

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -5,12 +7,22 @@ const tutorials = [
   'The Test Driven Development Workflow',
   'What is NaN and how Can we Check for it',
   'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
+        'Immutable State and Pure Functions',
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+    'what is JSONP?'
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+ 
+ 
+  return tutorials.map(tutorial => {
+    return tutorial
+      
+    
+    .split(" ")
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  });
+};
+
