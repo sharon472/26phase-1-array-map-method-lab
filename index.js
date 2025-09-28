5d020633126b6f52d907214b746b21f8e8309004
Sharon Njoroge <waithirasharon222@gmail>
2025-09-28 17:41:50 +0300

    add work

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const tutorials = [
+  
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
   'implementing Blockchain Web API',
   'The Test Driven Development Workflow',
   'What is NaN and how Can we Check for it',
   'What is the difference between stopPropagation and preventDefault?',
-        
+  'Immutable State and Pure Functions',
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
-    
+  'what is JSONP?'
 ];
+
 
 const titleCased = () => {
  
@@ -23,6 +25,5 @@ const titleCased = () => {
       .join(" ");
   });
 };
-
 
 
